197470306a5fd97daf164b2d6bddf5bf4b11abfe
Erwan NIQUET <eniquet@damart.com>
2024-03-08 15:24:39 +0100

    aws cred to env

--- awscredtoenv.sh
+++ awscredtoenv.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export AWS_ACCESS_KEY_ID=$(cat ../../.aws/credentials | grep "aws_access_key_id" | cut -d'=' -f2 | sed -e "s/ //")
-export AWS_SECRET_ACCESS_KEY=$(cat ../../.aws/credentials | grep "aws_secret_access_key" | cut -d'=' -f2 | sed -e "s/ //")
+export AWS_ACCESS_KEY_ID=$(awk -F "[[:space:]]*=[[:space:]]*" '/aws_access_key_id/{print $2}' $HOME/.aws/credentials)
+export AWS_SECRET_ACCESS_KEY=$(awk -F "[[:space:]]*=[[:space:]]*" '/aws_secret_access_key/{print $2}' $HOME/.aws/credentials)
